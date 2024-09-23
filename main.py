ab8a4d517ad31e1da89bd580fc1ef1e6debcdb4e
iriscarron <iris.carron@edu.esiee.fr>
2024-09-23 12:17:38 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-#### Imports et définition des variables globales
-
-
-#### Fonctions secondaires
-
-
+'''
+blabla
+'''
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne
+    de caractères passée en argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -13,14 +11,28 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    return [ ]
+    #condition d'arret
+    if not s:
+        return []
+    #code
+    liste=[]
+    caract = s[0]
+    c=0
+    for elt in s:
+        if elt == caract:
+            c+=1
+        else :
+            liste.append((caract,c))
+            caract=elt
+            c=1
+    liste.append((caract,c))
+    return liste
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant 
+    une chaîne de caractères passée en argument 
+    selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -28,22 +40,19 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
-
-    return []
-    
-
-#### Fonction principale
-
+    if not s:
+        return []
+    c=0
+    caract = s[0]
+    while c < len(s) and s[c] == caract:
+        c += 1
+    return [(caract, c)] + artcode_r(s[c:])
 
 def main():
+    """ main
+    """
     print(artcode_i('MMMMaaacXolloMM'))
-    print(artcode_r('MMMMaaacXolloMM'))
+    print(artcode_r('~~~~~@@@---@@@~~~~~'))
 
 if __name__ == "__main__":
     main()
